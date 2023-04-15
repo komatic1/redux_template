d69ef38677985257d5d72d971a95b47bcf857549
komatic1 <komatic.dda@gmail.com>
2023-04-15 11:29:49 +0300

    start project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const initialState = 0; // это state (состояние которое будет храниться в store)
+
+const reducer = (state, action) => {
+  return 0;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <></>
+  </React.StrictMode>
+);
